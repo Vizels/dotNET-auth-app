fbdd981868ba3a1881bdbca227f1f1334f4d8e43
Vizels <mirchuk.georgiy@gmail.com>
2024-06-04 19:02:08 +0200

    Add project to repository

---
+++ TestApp/Components/Movies/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthReviews.Components.Movies
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+        public float? Rate { get; set; }
+        public int? RatesNumber { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ TestApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AuthReviews.Components.Movies;
+
+namespace TestApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<AuthReviews.Components.Movies.Movie> Movie { get; set; } = default!;
+    }
+}
